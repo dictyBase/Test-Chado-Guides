4aeccd03bad792c289a1442dda6f86cc564ca2dd
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2013-06-19 11:50:23 -0500

    added WithMakeFile

---
+++ TestChado-WithMakeFile/lib/TestChado/WithMakeFile.pm
@@ -0,0 +1,111 @@
+package TestChado::WithMakeFile;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+TestChado::WithMakeFile - The great new TestChado::WithMakeFile!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use TestChado::WithMakeFile;
+
+    my $foo = TestChado::WithMakeFile->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Siddhartha Basu, C<< <sidd.basu at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-testchado-withmakefile at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=TestChado-WithMakeFile>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc TestChado::WithMakeFile
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=TestChado-WithMakeFile>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/TestChado-WithMakeFile>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/TestChado-WithMakeFile>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/TestChado-WithMakeFile/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 Siddhartha Basu.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of TestChado::WithMakeFile
